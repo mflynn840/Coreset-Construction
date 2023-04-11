db5adbdb0ebbe675ff5a427f75a3f7b0ec4c8861
mflynn840 <90886217+mflynn840@users.noreply.github.com>
2023-04-11 18:58:46 -0400

    idk  man

--- bruteforce2.py
+++ bruteforce2.py
@@ -1,10 +1,11 @@
 #Sure! Here's a better formatted list of the steps to construct a composable core set:
+import math
 import numpy as np
 from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
 
 class Coreset:
-    def __init__(self, originalDataset: list):
+    def __init__(self, originalDataset: tuple):
         self.originalDataset = originalDataset
         self.coreSet = []
 
@@ -15,19 +16,45 @@ class Coreset:
         #for each item in the steam
         for item in range(1, len(self.originalDataset)):
 
+            eligable = True
             #Compute the distance between the new data point and each point in the current core set.
             for cItem in self.coreSet:
-                x=1
+                if self.Idist(cItem, self.originalDataset[item]) > thresholdDistance:
+                    eligable = False
+                    break
+
+            if eligable:
+                self.coreSet.append(item)
+
+    def Idist(self, item1, item2):
+        return self.dist(item1[0], item1[1], item2[0], item2[1])
+    
+    def dist(self, x1:float, y1:float, x2:float, y2: float):
+        print(type(x2))
+        print(len(x2))
+        return math.sqrt((math.pow(x2-x1, 2)) + (math.pow(y2-y1, 2)))
+
+
+
+
 
 
 
 def testCoreSetConstruction():
     # Generate synthetic dataset
-    X, y = make_blobs(n_samples=100, centers=10, n_features=5, random_state=50)
+    X, y = make_blobs(n_samples=100, centers=10, n_features=1, random_state=50)
     print(X)
     print(y)
-    m=1
-    C = np.zeros(m)
+
+    coors = []
+    for i in range(0, len(X)):
+        coors.append((X[i][0], y[i]))
+    print("done")
+
+    coreset = Coreset(coors)
+
+    coreset.constructBruteForce(.8)
+    print(coreset.coreSet)
 
     plt.figure(figsize=(8, 6))  # Adjust the figure size to 8x6 inches
     plt.scatter(X[:, 0], X[:, 1], c=y)
