f7c452de28ea8cefb13c2ab43d3e9b2fc5ca8444
Fangyu Luo <fluo4@u.rochester.edu>
2023-04-26 23:16:18 -0400

    def CoresetConstruction(S, k):

--- Testbed.py
+++ Testbed.py
@@ -50,6 +50,44 @@ x = TestBed("Datasets/cifar-10-batches-py/data_batch")
 
     
 #    return S'
+def CoresetConstruction(S, k):
+    S_prime = []
+    sum_points = None
+
+    # Loop over each stream Si
+    for Si in S:
+        s = Si.getNext()
+
+        # if size of S' < k:
+        if len(S_prime) < k:
+            # add s to S'
+            S_prime.append(s)
+            if sum_points is None:
+                sum_points = s
+            else:
+                sum_points += s
+
+        else:
+            avg_points = sum_points / len(S_prime)
+
+            deviation = s - avg_points
+
+            min_dist = min([abs(s - p) for p in S_prime])
+
+            w = deviation / min_dist
+
+            w_prime = [deviation / min_dist for p in S_prime]
+
+            sum_w_prime = sum(w_prime)
+
+            # If adding s to S' makes div(S) "better", add s to S'
+            if w > sum_w_prime / len(S_prime):
+                S_prime.append(s)
+                sum_points += s
+                sum_points -= S_prime.pop(0)
+
+    return S_prime
+
 
 
 
