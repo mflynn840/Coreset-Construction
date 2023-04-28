15ae355bc14b925869f15c744b57551b6aace171
mflynn840 <90886217+mflynn840@users.noreply.github.com>
2023-04-28 02:40:37 -0400

    changes

--- LoadCIFARFile.py
+++ LoadCIFARFile.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import os
 
 #Turns cifar batches into numpy arrays
-class CifarLoader:
+class CifarDataset:
 
     def __init__(self, fileName: str):
         self.file = os.path.join(os.getcwd(), "Coreset-Construction", "Datasets", "cifar-10-batches-py", fileName)
@@ -23,9 +23,6 @@ class CifarLoader:
         rawImages = cifar_dict[b'data']
         self.labels = cifar_dict[b'labels']
 
-
-
-
         self.images = []
         # Reshape the image data to its original dimensions
         for i in range(0, len(rawImages)):
@@ -38,7 +35,17 @@ class CifarLoader:
         plt.title(f'CIFAR-10 Label: {self.labels[index]}')
         plt.show()
 
+    def getElement(self, index: int):
+        return (self.images[index], self.labels[index])
+
+    def getDataset(self):
+
+        dSet= set()
+        for i in range(0, len(self.labels)):
+            dSet.union(self.getElement(i))
+        
+        return dSet
 
 
-x = CifarLoader("data_batch_1")
-x.showImage(1)
+#x = CifarDataset("data_batch_1")
+#x.showImage(1)
